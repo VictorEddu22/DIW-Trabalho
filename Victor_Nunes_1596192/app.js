85aef7bfbcf5c2e69c22d783c13025e29b7f9f1b
VictorEdu <victor.edu.nunes2216@gmail.com>
2025-06-25 08:07:11 -0300

    Add files via upload

---
+++ Victor_Nunes_1596192/app.js
@@ -0,0 +1,21 @@
+const filmes = [
+    {
+      titulo: "A Origem",
+      genero: "Ficção Científica",
+      duracao: "2h 28min",
+      descricao: "Um ladrão que rouba segredos corporativos através do uso de sonhos compartilhados."
+    },
+    {
+      titulo: "Interestelar",
+      genero: "Drama, Ficção",
+      duracao: "2h 49min",
+      descricao: "Uma equipe viaja pelo espaço para garantir a sobrevivência da humanidade."
+      
+    },
+    {
+      titulo: "O Cavaleiro das Trevas",
+      genero: "Ação, Crime",
+      duracao: "2h 32min",
+      descricao: "Batman enfrenta o Coringa em Gotham City."
+    }
+  ];
